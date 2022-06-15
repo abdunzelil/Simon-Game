10e137405e74cd5e22d31d966648f1e58ebd2ff5
abdunzelil <93703261+abdunzelil@users.noreply.github.com>
2022-06-15 20:28:58 +0300

    Update game.js
    It is the last (and fully working) version

--- game.js
+++ game.js
@@ -4,28 +4,65 @@ var userClickedPattern = [];
 var level = 0;
 var firstTime = true;
 
-function nextSequence() {
-  $("h1").text("Level " + level);
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
-  level++;
-}
+$("body").keydown(function() {
+  if (firstTime) {
+    nextSequence();
+    firstTime = false;
+  }
+});
 
 $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  firstTime = true;
+}
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+    if (userClickedPattern.length === gamePattern.length) {
+
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+
+    }
+
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("h1").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
-
-
 }
 
 function animatePress (currentColour) {
@@ -35,13 +72,3 @@ function animatePress (currentColour) {
     100);
 }
 
-$("body").keydown(function() {
-  if (firstTime) {
-    nextSequence();
-    firstTime = false;
-  }
-});
-
-function checkAnswer(currentLevel) {
-
-}
